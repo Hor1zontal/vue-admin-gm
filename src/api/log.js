a2504d4ac3a3719d683f78e0cc9b143d10c472d0
彭若涛 <ruotao.peng@aliensidea.com>
2019-04-01 15:18:19 +0800

    修改配置文件，新增留存页面

---
+++ src/api/log.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function remainNewly(register, activity) {
+  return request({
+    url: '/remain/newly',
+    method: 'get',
+    data: {
+      reg: register,
+      activity: activity
+    }
+  })
+}
+
+export function remainActivity(activity, last_activity) {
+  return request({
+    url: '/remain/newly',
+    method: 'get',
+    data: {
+      activity: activity,
+      last_activity: last_activity
+    }
+  })
+}
---
+++ src/views/remain/newly/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="activity" style="padding:30px">
+    <div class="block" style="padding:20px;">
+      <span class="demonstration">用户注册的时间：</span>
+      <el-date-picker
+        v-model="value1"
+        :picker-options="pickerOptions"
+        align="right"
+        type="date"
+        placeholder="选择日期"/>
+    </div>
+    <div class="block" style="padding:20px;">
+      <span class="demonstration">最近活跃的时间：</span>
+      <el-date-picker
+        v-model="value2"
+        :picker-options="pickerOptions"
+        align="right"
+        type="date"
+        placeholder="选择日期"/>
+    </div>
+    <div style="padding:20px;">
+      <el-button type="primary" autofocus plain @click="searchOnClick()" >查询</el-button>
+    </div>
+    <div style="padding:20px;">
+      <span class="demonstration">注册人数：</span>
+      <el-tag type="success">{{ data1 }}</el-tag>
+    </div>
+    <div style="padding:20px;">
+      <span class="demonstration">活跃人数：</span>
+      <el-tag type="success">{{ data2 }}</el-tag>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: '今天',
+          onClick(picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: '昨天',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: '一周前',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
+      },
+      value1: '',
+      value2: '',
+      data1: 100,
+      data2: 100
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    searchOnClick: function() {
+      console.log('onClick')
+    },
+    init() {
+
+    }
+  }
+}
+</script>
